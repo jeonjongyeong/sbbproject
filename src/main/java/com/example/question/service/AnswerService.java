214fbe9d0e905de378d7f409694cd266b6c92e2d
jkkjkhh <jkkjkhh@gmail.com>
2022-07-23 15:36:56 +0900

    답변 등록

---
+++ src/main/java/com/example/question/service/AnswerService.java
@@ -0,0 +1,24 @@
+package com.example.question.service;
+
+import com.example.question.entity.Answer;
+import com.example.question.entity.Question;
+import com.example.question.repository.AnswerRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+
+@RequiredArgsConstructor
+@Service
+public class AnswerService {
+    private final AnswerRepository answerRepository;
+
+
+    public void create(Question question, String content) {
+        Answer answer = new Answer();
+        answer.setContent(content);
+        answer.setCreateDate(LocalDateTime.now());
+        answer.setQuestion(question);
+        this.answerRepository.save(answer);
+    }
+}
--- src/main/java/com/example/question/service/QuestionService.java
+++ src/main/java/com/example/question/service/QuestionService.java
@@ -1,7 +1,9 @@
 package com.example.question.service;
 
 import java.util.List;
+import java.util.Optional;
 
+import com.example.question.DataNotFoundException;
 import com.example.question.entity.Question;
 import com.example.question.repository.QuestionRepository;
 import org.springframework.stereotype.Service;
@@ -16,5 +18,14 @@ public class QuestionService {
 
     public List<Question> getList() {
         return this.questionRepository.findAll();
+    }
+
+    public Question getQuestion(Integer id) {
+        Optional<Question> question = this.questionRepository.findById(id);
+        if (question.isPresent()) {
+            return question.get();
+        } else {
+            throw new DataNotFoundException("question not found");
+        }
     }
 }
